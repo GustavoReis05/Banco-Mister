f79e1f198be7d179b670290f8174cb9d7652d65d
Gustavo Reis <gustavoreisg7462@gmail.com>
2026-08-28 16:43:13 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -7,3 +7,26 @@ print(CPF)
 print(email)
 print(login)
 print(senha)
+
+
+cpf = ""
+nome = ""
+login = ""
+senha = ""
+
+def cadastro_cliente (cpf, nome, login, senha):
+    cpf= input("CPF: ")
+    email = input("Email: ")
+    login = input("Login: ")
+    senha = input("Senha: ")
+
+
+
+    print(cpf)
+    print(nome)
+    print(login)
+    print(senha)
+
+
+print(cadastro_cliente)
+
