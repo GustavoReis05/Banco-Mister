47be5482ed1f1da0211eaf2ed0c3bf0ef3996929
Gustavo Reis <gustavoreisg7462@gmail.com>
2026-08-28 16:33:59 -0300

    Add balance management functions in conta.py

--- conta.py
+++ conta.py
@@ -1,1 +1,15 @@
+saldo = 0
 
+def consulta_saldo():
+    print("O saldo disponivel e: R$",saldo)
+
+def deposito(entrada):
+    saldo = saldo + entrada
+    return saldo
+
+def saque(saida):
+    if saida > saldo:
+        return "Valor indisponivel"
+    else:
+        saldo = saldo - saida
+        return "SAQUE REALIZADO COM SUCESSO!"
