86f08877f6aef8e0afde08995abc72b10457f64e
luisgustavojs <luis.gustavojss@gmail.com>
2026-08-31 21:02:29 -0300

    Atualizando o arquivo cliente.py

--- cliente.py
+++ cliente.py
@@ -1,32 +1,30 @@
-CPF = input("CPF: ")
-email = input("Email: ")
-login = input("Login: ")
-senha = input("Senha: ")
-
-print(CPF)
-print(email)
-print(login)
-print(senha)
-
-
-cpf = ""
-nome = ""
-login = ""
-senha = ""
-
-def cadastro_cliente (cpf, nome, login, senha):
+def cadastro_cliente ():
     cpf= input("CPF: ")
-    email = input("Email: ")
+    nome = input("Nome completo: ")
     login = input("Login: ")
     senha = input("Senha: ")
 
+    print("\n")
 
+    print("Os dados estão corretos?")
+    print("\n")
+    print("CPF: ",cpf)
+    print("Nome completo: ",nome)
+    print("Login: ",login)
+    print("Senha: ",senha)
 
-    print(cpf)
-    print(nome)
-    print(login)
-    print(senha)
+    print("\n")
 
-
-print(cadastro_cliente)
+    confirmacao = int(input("DIGITE A OPÇÃO DE ACORDO COM A VALIDADE DOS SEUS DADOS: \n"
+                            "1 - Sim, os dados estão corretos.\n" \
+                            "2 - Não, os dados estão incorretos\n"))
+    if confirmacao == 1:
+        print("CONTA CRIADA COM SUCESSO!")
+        return nome
+    elif confirmacao == 2:
+        print("QUE PENA! REINICIE O PROGRAMA PARA CRIARMOS SUA CONTA COM OS DADOS CORRETOS.")
+        exit()
+    else:
+        print("OPÇÃO INVALIDA!")
+        exit()
 
