3ba5776a76c7cc3ef1398dba1eacec730c120c39
Gustavo Reis <gustavoreisg7462@gmail.com>
2026-08-31 21:25:13 -0300

    Criando as funcinalidades da conta

--- conta.py
+++ conta.py
@@ -1,15 +1,26 @@
-saldo = 0
+"""Implantação de um menu para escolha da operação desejada"""
+def operacao(saldo):
+    menu_inicial = int(input("1 - CONSULTAR SALDO\n" 
+                            "2 - REALIZAR DEPÓSITO\n"
+                            "3 - REALIZAR SAQUE\n"))
+    if menu_inicial == 1:
+        print("O saldo disponivel é: R$",saldo)
 
-def consulta_saldo():
-    print("O saldo disponivel é: R$",saldo)
+    elif menu_inicial == 2:
+        valor_dep = float(input("Valor do depósito: R$"))
+        saldo = saldo + valor_dep
+        print("Deposito realizado com sucesso!")
 
-def deposito(entrada):
-    saldo = saldo + entrada
-    return saldo
+    elif menu_inicial == 3:
+        valor_saque = float(input("Valor do saque: R$"))
 
-def saque(saida):
-    if saida > saldo:
-        return "Valor indisponivel"
+        if valor_saque > saldo:
+            print("Valor indisponivel")
+
+        else:
+            saldo = saldo - valor_saque
+            print("SAQUE REALIZADO COM SUCESSO!") 
+
     else:
-        saldo = saldo - saida
-        return "SAQUE REALIZADO COM SUCESSO!"
+        print("OPERAÇÃO INVALIDA.")
+        
