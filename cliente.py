3866a0c4e1dce9bccf295eed8323e8f6d339d09a
Gustavo Reis <gustavoreisg7462@gmail.com>
2026-09-17 16:05:41 -0300

    Ajuste na lógica na funçao de cadastro do cliente

--- cliente.py
+++ cliente.py
@@ -1,36 +1,37 @@
 def cadastrar_cliente():
-    cpf= input("CPF: \n")
-    nome = input("Nome completo: \n")
-    data_nascimento = input("Data de nascimento: \n")
-    numero_telefone = input("Número de telefone: \n")
+    while True:
+        cpf= input("CPF: \n")
+        nome = input("Nome completo: \n")
+        data_nascimento = input("Data de nascimento: \n")
+        numero_telefone = input("Número de telefone: \n")
 
 
-    print("\n")
-    
-    print("Os dados estão corretos?")
-    print("\n")
-    print("CPF: ",cpf)
-    print("Nome completo: ",nome)
-    print("Data de Nascimento: ", data_nascimento)
-    print("Numero de Telefone: ", numero_telefone)
-    
-    print("\n")
-    
-    confirmacao = int(input("DIGITE A OPÇÃO DE ACORDO COM A VALIDADE DOS SEUS DADOS: \n"
-                            "1 - Sim, os dados estão corretos.\n" \
-                            "2 - Não, os dados estão incorretos\n"))
+        print("\n")
+        
+        print("Os dados estão corretos?")
+        print("\n")
+        print("CPF: ",cpf)
+        print("Nome completo: ",nome)
+        print("Data de Nascimento: ", data_nascimento)
+        print("Numero de Telefone: ", numero_telefone)
+        
+        print("\n")
+        
+        confirmacao = int(input("DIGITE A OPÇÃO DE ACORDO COM A VALIDADE DOS SEUS DADOS: \n"
+                                "1 - Sim, os dados estão corretos.\n" \
+                                "2 - Não, os dados estão incorretos\n"))
 
-    while confirmacao == 2:
-        print("CADASTRE SEUS DADOS NOVAMENTE!")
-        return cadastrar_cliente()
+        if confirmacao == 1:
+            print("CLIENTE CADASTRADO COM SUCESSO!")
+            return cpf, nome, data_nascimento, numero_telefone                 
 
-    if confirmacao == 1:
-        print("CLIENTE CADASTRADO COM SUCESSO!")
-        return cpf, nome, data_nascimento, numero_telefone
-        exit()
+        elif confirmacao == 2:
+            print("CADASTRE SEUS DADOS NOVAMENTE!")
+            return cadastrar_cliente()
+            exit()
 
 def procurar_cliente(lista_clientes, cpf):
-    """Busca um cliente na lista pelo cpf"""
+    """Busca um cliente pelo cpf"""
     for cliente in lista_clientes:
         if cliente[0] == cpf:
             return cliente
