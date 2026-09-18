3636e9d55cc80d06899d50865bf4273e72427454
luisgustavojs <luis.gustavojss@gmail.com>
2026-09-17 22:09:13 -0300

    Corrigindo um bug no json pelo main

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ lista_clientes = []
 lista_agencias = []
 lista_contas = []
 
+
+
 def menu_principal():
     # Carrega os dados persistidos na inicialização
     conta.carregar_dados_json(lista_clientes, lista_agencias, lista_contas)
 
-
-def menu_principal():
 
     while True:
         print("\n=== BANCO MISTER ===")
